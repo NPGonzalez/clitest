c619b92a87ceb7eadd39b410aaf593538b0791df
NPGonzalez <naypili@gmail.com>
2020-09-19 23:37:29 -0300

    Se trabaja en el constructor de home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log('Hola desde AuthService');
+  }
 
   ngOnInit(): void {
   }
